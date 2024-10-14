3ca6df1514719ec84235ed6a86e41d232b5cd034
Joshua Nanninga <jmarknanninga@gmail.com>
2024-10-14 16:16:25 -0500

    final cleanup, commit

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./api/userRoutes');
-const thoughtRoutes = require('./api/thoughtRoutes');
-
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
-
-module.exports = router;
--- utils/dateFormat.js
+++
@@ -1,5 +0,0 @@
-// utils/dateFormat.js
-module.exports = (timestamp) => {
-    return new Date(timestamp).toLocaleString();
-  };
-  
